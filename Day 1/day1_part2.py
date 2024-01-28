0faefe70d607ea312dbe044cbeb9a23daa31b501
Yash Jain <1999yashjain@gmail.com>
2024-01-27 20:12:36 -0500

    Day 1 cleaned

--- Day 1/day1_part2.py
+++ Day 1/day1_part2.py
@@ -13,16 +13,6 @@ num_map={
     'seven': '7', 
     'eight': '8', 
     'nine': '9',
-    # '0':'0',
-    # '1':'1',
-    # '2':'2',
-    # '3':'3',
-    # '4':'4',
-    # '5':'5',
-    # '6':'6',
-    # '7':'7',
-    # '8':'8',
-    # '9':'9'
 }
 
 final_val=0
@@ -36,15 +26,8 @@ for line in input_lines:
             for num_text in num_map:
                 if line[idx:].startswith(num_text):
                     idxs.append([num_map[num_text], idx])
-        
-    # for num_text in num_map:
-    #     if num_text in line:
-    #         idxs.append([num_text, line.rfind(num_text)])
     idxs=sorted(idxs, key=lambda k: k[1])
-    # print(idxs)
     final_val+= int(idxs[0][0]+idxs[-1][0])
     final_nums.append(int(idxs[0][0]+idxs[-1][0]))
 
-# with open('./Day 1/txt2.txt', 'r+') as outfile:
-#     outfile.write(str(final_nums))
 print(final_val)
